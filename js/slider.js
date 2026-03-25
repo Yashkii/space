5c47bbc5c1e07669a12eb1a61b6c6cc776d2076d
Yashki <108458308+Yashkii@users.noreply.github.com>
2026-03-25 17:23:39 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const slider = document.querySelector('.slider')
+slider.onclick = nextSlide
+const slideTimeout = 2400
+let sliderTimerId = setTimeout(nextSlide, slideTimeout)
+
+const images = slider.querySelectorAll('img')
+console.log(slider, images)
+
+function nextSlide() {
+    clearInterval(sliderTimerId)
+
+    // перемещаем просмотренный слайд в начало
+    const movedSlide = slider.querySelector('.move-left')
+    if (movedSlide) {
+        movedSlide.classList.remove('move-left')
+        movedSlide.remove()
+        slider.prepend(movedSlide)
+    }
+
+    const images = slider.querySelectorAll('img')
+    images[images.length - 1].classList.add('move-left')
+    
+    sliderTimerId = setTimeout(nextSlide, slideTimeout)   
+}
